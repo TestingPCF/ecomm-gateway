d43bd96b6544504ade5b64089ab9fe7bbd4f679d
Abhishek Singh <abhishek_sin@HCL.COM>
2019-04-26 11:54:35 +0530

    ecomm-gateway

---
+++ src/test/java/com/hcl/cloud/user/gateway/HospitalGatewayApplicationTests.java
@@ -0,0 +1,18 @@
+package com.hcl.cloud.user.gateway;
+/*package com.javatechie.spring.zulu.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HospitalGatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+*/
